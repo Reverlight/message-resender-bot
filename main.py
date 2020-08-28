97e253533a75a01a5aaae35af3e7678d7275d858
reverlight <artem3766@gmail.com>
2020-08-28 15:33:15 +0300

    Added simple message handling

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
+import telebot
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')  # Here is your's TELEGRAM BOT API TOKEN
+print(TELEGRAM_BOT_TOKEN)
+
+bot = telebot.TeleBot("TOKEN")
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Напишите сообщение, оно будет сразу отправлено админу")
+
+
+bot.polling()
