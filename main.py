53f53a61319bdd746d8a6066597cc95cbaeae88c
reverlight <artem3766@gmail.com>
2020-08-28 16:29:48 +0300

    Changed variables order and message

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_instructions(message):
-
     this_chat_id = message.chat.id
     print(this_chat_id)
-    bot.send_message(this_chat_id, "Напишите сообщение, оно будет сразу отправлено админу")
+    bot.send_message(this_chat_id, "Напишите сообщение, оно будет сразу отправлено админу. Потом с вами свяжиться в лс админ @suicidenjoy")
 
 
 @bot.message_handler(func=lambda message: True)
 def forward_message(message):
     from_chat_id = message.chat.id
-    this_message_id = message.message_id
     to_chat_id = ADMIN_CHAT_ID
+    this_message_id = message.message_id
     bot.forward_message(to_chat_id, from_chat_id, this_message_id)
+
 
 bot.polling()
