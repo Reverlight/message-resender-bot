f2123729b9e7f9468f94738ebfc33930810c6644
reverlight <artem3766@gmail.com>
2020-08-28 15:47:23 +0300

    Added ADMIN_CHAT_ID for person who bot will forward messages to Added this_chat_id and this_message_id variables.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import telebot
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')  # Here is your's TELEGRAM BOT API TOKEN
+ADMIN_CHAT_ID = os.environ.get('ADMIN_CHAT_ID')  # Here is Chat Id of admin who you want send messages to.
 
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
@@ -14,8 +15,10 @@ bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Напишите сообщение, оно будет сразу отправлено админу")
-
+    this_chat_id = message.chat.id
+    this_message_id = message.id
+    print(this_chat_id)
+    bot.send_message(this_chat_id, "Напишите сообщение, оно будет сразу отправлено админу")
+    
 
 bot.polling()
